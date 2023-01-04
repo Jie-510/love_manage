fb52c815343106400ed0c7e2856490a8a5e4127f
Jie <320358490@qq.com>
2023-01-04 22:17:12 +0800

    工程化项目

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+// 初始化css
+import '@/assets/styles/index.scss'
+// svg注册脚本
+import 'virtual:svg-icons-register'
 
-createApp(App).mount('#app')
+// 分解app
+const app = createApp(App)
+// 全局注册svg
+import SvgIcon from '@/components/SvgIcon.vue';
+app.component('SvgIcon', SvgIcon)
+
+
+
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // svg使用
+    createSvgIconsPlugin({
+      iconDirs: [resolve(process.cwd(), './src/assets/icons')],
+      symbolId: '[name]'
+    })
+  ],
+  // 路径别名
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, 'src')
+    }
+  }
 })
